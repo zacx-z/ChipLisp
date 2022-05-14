617dd1612e7e32329f6240f047fcae7a24f47208
ladace <ladacez@gmail.com>
2022-05-14 13:24:42 +0800

    added clr test (incomplete)

--- Test/Tests.cs
+++ Test/Tests.cs
@@ -2,6 +2,7 @@
 using Xunit;
 using Nela.ChipLisp;
 using Nela.ChipLisp.LangExtensions;
+using Nela.ChipLisp.Libs;
 
 namespace Test {
     public class Tests {
@@ -9,14 +10,14 @@ namespace Test {
         public void TestAdd() {
             var state = new State();
             state.LoadPreludeLib();
-            Assert.Equal(state.Eval("(+ 1 2)"), ValueObj.Create(3));
+            Assert.Equal(ValueObj.Create(3), state.Eval("(+ 1 2)"));
         }
 
         [Fact]
         public void TestRecursion() {
             var state = new State();
             state.LoadPreludeLib();
-            Assert.Equal(state.Eval("(progn (defun sum (a . b) (if b (+ a (sum . b)) a)) (sum 1 2 3 4))"), ValueObj.Create(10));
+            Assert.Equal(ValueObj.Create(10), state.Eval("(progn (defun sum (a . b) (if b (+ a (sum . b)) a)) (sum 1 2 3 4))"));
         }
 
         [Fact]
@@ -24,7 +25,7 @@ namespace Test {
             var state = new State();
             state.LoadPreludeLib();
             var parser = new ExtendedParser();
-            Assert.Equal(state.Eval(parser.ReadExpr(new Lexer(new StringReader(
+            Assert.Equal(ValueObj.Create(10), state.Eval(parser.ReadExpr(new Lexer(new StringReader(
 @"(progn
 (defmacro plus-all args
     (if (cdr args)
@@ -32,7 +33,17 @@ namespace Test {
         (car args))
 )
 (eval (plus-all 1 2 3 4))
-)")))), ValueObj.Create(10));
+)")))));
+        }
+
+        [Fact]
+        public void TestCLR() {
+            var state = new State();
+            state.LoadPreludeLib();
+            state.LoadLib(ClrLib.Load);
+            Assert.Equal(ValueObj.Create(typeof(string)), state.Eval("(progn (clr-using \"System\") (clr-type \"String\"))"));
+            Assert.Equal(ValueObj.Create(typeof(string)), state.Eval("(clr-type \"System.String\")"));
+            //Assert.Equal(ValueObj.Create(new string[]{"a", "b", "c"}), state.Eval("(clr-call-member \"a,b,c\" 'Split \",\" (clr-call (clr-type \"StringSplitOptions\") '@None ()))"));
         }
     }
 }
